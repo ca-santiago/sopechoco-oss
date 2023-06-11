06b6ea8368acb5dc3f2ae7a05a45b0e7d7fddcab
Carmen Santiago <ca.santiago.me@gmail.com>
2023-06-11 13:34:51 -0600

    feat: add tailwind fixes and app name

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View className="flex flex-row bg-red-200">
+          <View className="w-3/12">
+            <Text className="bg-red-100 text-xl">One</Text>
+          </View>
+          <View className="w-9/12">
+            <Text>Two</Text>
+          </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
